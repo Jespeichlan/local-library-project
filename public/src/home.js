1e43fcf8725f0c550ee2eb911467490f233d093f
Jesse Speicher <jespeichlan@gmail.com>
2022-02-28 19:16:16 -0600

    test commit

--- public/src/home.js
+++ public/src/home.js
@@ -1,6 +1,12 @@
-function getTotalBooksCount(books) {}
+function getTotalBooksCount(books) {
+  const totalBooks = books.length;
+  return totalBooks;
+}
 
-function getTotalAccountsCount(accounts) {}
+function getTotalAccountsCount(accounts) {
+  const totalAccounts = accounts.length;
+  return totalAccounts;
+}
 
 function getBooksBorrowedCount(books) {}
 
@@ -8,7 +14,27 @@ function getMostCommonGenres(books) {}
 
 function getMostPopularBooks(books) {}
 
-function getMostPopularAuthors(books, authors) {}
+function getMostPopularAuthors(books, authors) {
+  const popularAuthors = [];
+  for (let author of authors){
+    const authorName = `${author.name.first} ${author.name.last}`;
+    let count = 0;
+    for (let book of books){
+      if (author.id === book.authorId){
+        count += book.borrows.length;
+      }
+    }
+    const authorObj = {name :authorName, count: count};
+    popularAuthors.push(authorObj);
+  }
+}
+
+
+
+function topFive(array){
+  let result = array.sort((countA, countB) => (countA.count < countB.count ? 1: -1)).slice(0,5);
+  return result;
+}
 
 module.exports = {
   getTotalBooksCount,
